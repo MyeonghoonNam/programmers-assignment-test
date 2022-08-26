3041d44b25d71fcb8bf11e9b79103dfc95b42153
MyeongHoonNam <codenmh0822@naver.com>
2022-08-26 20:05:48 +0900

    feat: router 구현

---
+++ shoppingmall-spa/src/router/router.js
@@ -0,0 +1,68 @@
+import getClosestElement from '../utils/getClosestElement.js';
+import { store } from '../store/store.js';
+
+const Router = (routes) => {
+	const ROUTES = { ...routes };
+	let $root;
+
+	window.addEventListener('click', (e) => {
+		const $element = getClosestElement(e.target, 'li');
+
+		if ($element?.className === 'Product') {
+			const productId = $element.dataset.id;
+
+			store.setState();
+			routePush(`/web/products/${productId}`);
+		}
+	});
+
+	window.onpopstate = () => {
+		store.setState();
+		routing(window.location.pathname, 'Routing');
+	};
+
+	const routePush = (pathname) => {
+		window.history.pushState({}, null, pathname);
+		routing(pathname, 'Routing');
+	};
+
+	const routing = async (pathname, mode = 'StateChange') => {
+		const [, prefix, routename, param] = pathname.split('/');
+		let page;
+
+		if (ROUTES[pathname]) {
+			page = await ROUTES[pathname]($root);
+		} else if (param) {
+			page = await ROUTES[`/${prefix}/${routename}/:productId`]($root, param);
+		}
+
+		if (page) {
+			if (mode === 'Routing') {
+				render(page);
+			} else if (mode === 'StateChange') {
+				return page;
+			}
+		} else {
+			// page = not found Page
+		}
+
+		return false;
+	};
+
+	const render = (page) => {
+		const $target = $root;
+
+		$target.innerHTML = '';
+		$target.appendChild(page);
+	};
+
+	return (target) => {
+		$root = target;
+
+		const { pathname } = window.location;
+		const $element = routing(pathname);
+		return $element;
+	};
+};
+
+export default Router;

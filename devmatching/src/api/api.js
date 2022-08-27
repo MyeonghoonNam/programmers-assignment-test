ea8a39525522bafff18e04f3230f3bb902c43967
MyeongHoonNam <codenmh0822@naver.com>
2022-08-27 22:39:15 +0900

    feat: api 호출 래핑 객체 구현

---
+++ devmatching/src/api/api.js
@@ -0,0 +1,49 @@
+const parseResponse = async (response) => {
+	const { status } = response;
+	let data;
+
+	if (status !== 204) {
+		data = await response.json();
+	}
+
+	return { status, data };
+};
+
+const request = async (params) => {
+	try {
+		const { url, headers = {}, body, method = 'GET' } = params;
+		const config = {
+			method,
+			headers,
+		};
+
+		if (body) {
+			config.body = JSON.stringify(body);
+		}
+
+		const response = await fetch(url, config);
+
+		if (response.ok) {
+			return parseResponse(response);
+		}
+
+		throw new Error('API 통신 오류');
+	} catch (e) {
+		console.log(e.message);
+		return false;
+	}
+};
+
+const get = async (url, headers) => {
+	const response = await request({
+		url,
+		headers,
+		method: 'GET',
+	});
+
+	return response.data;
+};
+
+export default {
+	get,
+};

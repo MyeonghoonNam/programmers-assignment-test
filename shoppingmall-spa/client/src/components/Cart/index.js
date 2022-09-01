82c028d2b7cab485623245aa2596e1a44886d3ea
MyeongHoonNam <codenmh0822@naver.com>
2022-09-01 17:39:26 +0900

    feat: Cart 컴포넌트 동적 렌더링 구현

--- shoppingmall-spa/client/src/components/Cart/index.js
+++ shoppingmall-spa/client/src/components/Cart/index.js
@@ -1,36 +1,63 @@
+import { store } from '../../store/store.js';
+import getNumberWithCommas from '../../utils/getNumberWithCommas.js';
+
 const Cart = () => {
+	let state;
+
+	const getTotalPrice = () => {
+		const { cartProductList } = state;
+
+		const price = cartProductList.reduce(
+			(acc, product) =>
+				acc + (product.productPrice + product.optionPrice) * product.quantity,
+			0,
+		);
+
+		return price;
+	};
+
 	const render = () => {
+		const { cartProductList } = state;
+
 		const $cart = document.createElement('div');
 		$cart.setAttribute('class', 'Cart');
 		$cart.innerHTML = `
-      <div class="Cart">
-        <ul>
-          <li class="Cart__item">
-            <img src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png">
-            <div class="Cart__itemDesription">
-              <div>커피잔 100개 번들 10,000원 10개</div>
-              <div>100,000원</div>
-            </div>
-          </li>
-          <li class="Cart__item">
-            <img src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png">
-            <div class="Cart__itemDesription">
-              <div>커피잔 1000개 번들 15,000원 5개</div>
-              <div>75,000원</div>
-            </div>
-          </li>
-        </ul>
+      <ul>
+      ${cartProductList
+				.map(
+					({
+						imageUrl,
+						productName,
+						productPrice,
+						optionName,
+						optionPrice,
+						quantity,
+					}) => `
+        <li class="Cart__item">
+          <img src="${imageUrl}">
+          <div class="Cart__itemDesription">
+            <div>${productName} ${optionName} ${quantity}개</div>
+            <div>${getNumberWithCommas(productPrice + optionPrice)}원</div>
+          </div>
+        </li>
+        `,
+				)
+				.join('')}
+      </ul>
 
-        <div class="Cart__totalPrice">
-          총 상품가격 175,000원
-        </div>
-
-        <button class="OrderButton">주문하기</button>
+      <div class="Cart__totalPrice">
+        총 상품가격 ${getNumberWithCommas(getTotalPrice())}원
       </div>
+
+      <button class="OrderButton">주문하기</button>
     `;
+
+		return $cart;
 	};
 
 	return () => {
+		state = store.getState();
+		getTotalPrice();
 		const $cart = render();
 		return $cart;
 	};

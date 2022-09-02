721e91c3fd888b3a9624056093697ae1b5ddc7d6
MyeongHoonNam <codenmh0822@naver.com>
2022-09-02 21:18:51 +0900

    feat: store init

---
+++ programming-language-search/client/src/store/store.js
@@ -0,0 +1,66 @@
+import { reducer } from './reducer.js';
+import getCloneDeepObject from '../utils/getCloneDeepObject.js';
+
+const CreateStore = () => {
+	const listeners = [];
+	let state = reducer();
+
+	const subscribe = (listener) => {
+		listeners.push(listener);
+	};
+
+	const publish = () => {
+		const payload = Object.freeze(state);
+		listeners.forEach((listener) => listener(payload));
+	};
+
+	const dispatch = (action) => {
+		const newState = reducer(state, action);
+		state = newState;
+		publish();
+	};
+
+	const getState = () => Object.freeze(state);
+
+	const setState = (payload) => {
+		if (!payload) {
+			state = reducer();
+			return;
+		}
+
+		const keys = Object.keys(payload);
+
+		for (let i = 0; i < keys.length; i += 1) {
+			const key = keys[i];
+
+			if (!key) return;
+		}
+
+		state = getCloneDeepObject({
+			...state,
+			...payload,
+		});
+	};
+
+	const isEmpty = (value) => {
+		if (Array.isArray(value)) {
+			return value.length === 0;
+		}
+
+		if (typeof value === 'object') {
+			return Object.keys(value).length === 0;
+		}
+
+		return new Error('잘못된 데이터 타입을 조회하였습니다.');
+	};
+
+	return {
+		subscribe,
+		dispatch,
+		getState,
+		setState,
+		isEmpty,
+	};
+};
+
+export const store = CreateStore();

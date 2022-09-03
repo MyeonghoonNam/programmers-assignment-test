f036c6605638b52876b0077849f228abb71f0c7a
MyeongHoonNam <codenmh0822@naver.com>
2022-09-03 12:26:38 +0900

    feat: api 래퍼 객체 및 get 요청 구현

---
+++ programming-language-search/client/src/api/api.js
@@ -0,0 +1,50 @@
+const parseResponse = async (response) => {
+	const { status } = response;
+	let data;
+
+	if (status !== 204) {
+		data = await response.json();
+	}
+
+	return { status, data };
+};
+
+const request = async (params) => {
+	try {
+		const { url, method = 'GET', headers = {}, body } = params;
+		const config = {
+			method,
+			headers,
+		};
+
+		if (body) {
+			config.body = JSON.stringify(body);
+		}
+
+		const response = await fetch(url, config);
+
+		if (response.ok) {
+			return parseResponse(response);
+		}
+
+		throw new Error('API 오류');
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.log(e.message);
+		return false;
+	}
+};
+
+const get = async (url, headers) => {
+	const response = await request({
+		url,
+		method: 'GET',
+		headers,
+	});
+
+	return response.data;
+};
+
+export default {
+	get,
+};

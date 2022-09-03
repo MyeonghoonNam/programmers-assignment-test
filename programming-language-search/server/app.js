e6c19f476a8fb70c7726f082a9adf19329d539c7
MyeongHoonNam <codenmh0822@naver.com>
2022-09-03 13:00:51 +0900

    feat: languages get 요청 구현

--- programming-language-search/server/app.js
+++ programming-language-search/server/app.js
@@ -2,9 +2,34 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+const LANGUAGES = [
+  "Javascript",
+  "java",
+  "typescript",
+  "php",
+  "Asp",
+  "Jsp",
+  "React",
+  "Python",
+  "nodejs",
+  "AnelScript",
+  "CobolScript",
+  "json",
+  "jsonjava",
+];
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get("/languages", (req, res) => {
+  const { keyword } = req.query;
+  const data = LANGUAGES.filter((language) =>
+    language.toLowerCase().includes(keyword)
+  );
+
+  res.send(data);
+});
 
 export default app;

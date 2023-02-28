2b75e7436b0c0c8a44bb13e56f80de16dedbacbe
MyeongHoonNam <codenmh0822@naver.com>
2023-02-28 18:36:35 +0900

    feat : url에 따른 페이지 렌더링 구현

--- personnel-information-spa-renewal/src/App.js
+++ personnel-information-spa-renewal/src/App.js
@@ -1,4 +1,6 @@
 import Header from "./components/Header.js";
+import HomePage from "./page/HomePage.js";
+import SignupPage from "./page/SignupPage.js";
 
 class App {
   constructor(container) {
@@ -8,8 +10,32 @@ class App {
 
   render() {
     const $header = new Header(this.$container);
-
     $header.render();
+
+    const $main = document.createElement("main");
+    $main.setAttribute("id", "page_content");
+    this.$container.appendChild($main);
+
+    const homePage = new HomePage($main);
+    const signupPage = new SignupPage($main);
+
+    homePage.render();
+
+    document.addEventListener("urlChange", (e) => {
+      let pathname = e.detail.href;
+
+      $main.innerHTML = "";
+
+      switch (pathname) {
+        case "/web/":
+          homePage.render();
+          break;
+        case "/web/signup":
+          signupPage.render();
+          break;
+        default:
+      }
+    });
   }
 }
 

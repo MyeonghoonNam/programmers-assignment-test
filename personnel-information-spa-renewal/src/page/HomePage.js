aad2bf337f05e03e5f1e37e686bfec58d259ef63
MyeongHoonNam <codenmh0822@naver.com>
2023-03-01 20:42:41 +0900

    feat : HomePage에 Cards 컴포넌트 렌더링 적용

--- personnel-information-spa-renewal/src/page/HomePage.js
+++ personnel-information-spa-renewal/src/page/HomePage.js
@@ -1,4 +1,5 @@
 import ContentTitle from "../components/ContentTitle.js";
+import Cards from "../components/Cards.js";
 
 class HomePage {
   constructor(container) {
@@ -7,8 +8,10 @@ class HomePage {
 
   render() {
     const $title = new ContentTitle(this.$container, "Great PeoPle");
+    const $cards = new Cards(this.$container);
 
     $title.render();
+    $cards.render();
   }
 }
 

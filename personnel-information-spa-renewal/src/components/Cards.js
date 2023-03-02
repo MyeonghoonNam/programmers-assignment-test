6651332cf316caf5b09b67a2585fb6793be129b2
MyeongHoonNam <codenmh0822@naver.com>
2023-03-02 15:18:05 +0900

    feat : 무한스크롤 UI 구현

--- personnel-information-spa-renewal/src/components/Cards.js
+++ personnel-information-spa-renewal/src/components/Cards.js
@@ -4,6 +4,48 @@ class Cards {
   constructor(container) {
     this.$container = container;
     this.personalInfo = JSON.parse(localStorage.getItem("personalInfo"));
+    this.cardIncrementInterval = 4;
+    this.cardIncrementIntervalCount = 2;
+    this.currentCardCount = 0;
+    this.totalCardCount = this.personalInfo.length;
+  }
+
+  infiniteScroll(container) {
+    let target = container.lastChild;
+
+    const io = new IntersectionObserver(
+      (entry, observer) => {
+        if (entry[0].isIntersecting) {
+          observer.unobserve(target);
+
+          for (
+            let i = this.currentCardCount;
+            i < this.cardIncrementInterval * this.cardIncrementIntervalCount;
+            i++
+          ) {
+            if (this.personalInfo[i]) {
+              const { idx, nickname, mbti } = this.personalInfo[i];
+              const card = new Card(container, idx, nickname, mbti);
+
+              card.render();
+              this.currentCardCount++;
+            }
+          }
+
+          this.cardIncrementIntervalCount++;
+          target = container.lastChild;
+
+          if (this.currentCardCount !== this.totalCardCount) {
+            observer.observe(target);
+          }
+        }
+      },
+      {
+        threshold: 0.7,
+      }
+    );
+
+    io.observe(target);
   }
 
   render() {
@@ -25,11 +67,15 @@ class Cards {
       localStorage.setItem("cardStatus", JSON.stringify(cardStatus));
     });
 
-    this.personalInfo.forEach(({ idx, nickname, mbti }) => {
+    for (let i = 0; i < this.cardIncrementInterval; i++) {
+      const { idx, nickname, mbti } = this.personalInfo[i];
       const card = new Card($div, idx, nickname, mbti);
-      card.render();
-    });
 
+      card.render();
+      this.currentCardCount++;
+    }
+
+    this.infiniteScroll($div);
     this.$container.appendChild($div);
   }
 }
